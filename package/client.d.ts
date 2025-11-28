deeb893f6ab0c1e7c890007cc229768c7809669f
DevTobias <github@tobiaskaerst.dev>
2025-11-28 23:24:03 +0100

    feat: v1.1.0 release

--- package/client.d.ts
+++ package/client.d.ts
@@ -2,6 +2,7 @@ declare module "virtual:oss-licenses" {
   const licenses: {
     name: string;
     version: string;
+    isDirectDependency: boolean;
     description: string | null;
     homepage: string | null;
     repository: string | null;
